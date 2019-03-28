4e1a9c3c4a45f81fb09a60b7e3db5183c23bc923
Adrian Heine <mail@adrianheine.de>
2019-03-28 21:22:41 +0100

    Pull upstream mediamanager.php

--- mediamanager.php
+++ mediamanager.php
@@ -1,44 +1,44 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
- "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <?php
 /**
- * DokuWiki Default Template
+ * DokuWiki Media Manager Popup
  *
- * This is the template for the media manager popup
- *
- * You should leave the doctype at the very top - It should
- * always be the very first line of a document.
- *
- * @link   http://dokuwiki.org/templates
- * @author Andreas Gohr <andi@splitbrain.org>
+ * @author   Andreas Gohr <andi@splitbrain.org>
+ * @license  GPL 2 (http://www.gnu.org/licenses/gpl.html)
  */
-?>
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $conf['lang']?>" lang="<?php echo $conf['lang']?>" dir="ltr">
+// must be run from within DokuWiki
+if (!defined('DOKU_INC')) die();
+header('X-UA-Compatible: IE=edge,chrome=1');
+
+?><!DOCTYPE html>
+<html lang="<?php echo $conf['lang']?>" dir="<?php echo $lang['direction'] ?>" class="popup no-js">
 <head>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta charset="utf-8" />
   <title>
     <?php echo hsc($lang['mediaselect'])?>
     [<?php echo strip_tags($conf['title'])?>]
   </title>
+    <script>(function(H){H.className=H.className.replace(/\bno-js\b/,'js')})(document.documentElement)</script>
   <?php tpl_metaheaders()?>
-  <link rel="shortcut icon" href="<?php echo DOKU_TPL?>images/favicon.ico" />
+    <meta name="viewport" content="width=device-width,initial-scale=1" />
+    <?php echo tpl_favicon(array('favicon', 'mobile')) ?>
+    <?php tpl_includeFile('meta.html') ?>
 </head>
 
 <body>
 <div id="media__manager" class="dokuwiki">
-    <div id="media__left">
-        <?php html_msgarea()?>
+        <?php html_msgarea() ?>
+        <div id="mediamgr__aside"><div class="pad">
         <h1><?php echo hsc($lang['mediaselect'])?></h1>
 
         <?php /* keep the id! additional elements are inserted via JS here */?>
         <div id="media__opts"></div>
 
         <?php tpl_mediaTree() ?>
-    </div>
+        </div></div>
 
-    <div id="media__right">
+        <div id="mediamgr__content"><div class="pad">
         <?php tpl_mediaContent() ?>
-    </div>
+        </div></div>
 </div>
 </body>
 </html>
